5241245c337aa9dafde0a0a72667e09cff136887
andrestolher <andrestolher@gmail.com>
2026-08-11 14:39:15 -0600

    Match persistent session schema

--- server/src/prisma/migrations/20260811000000_add_email_auth/migration.sql
+++ server/src/prisma/migrations/20260811000000_add_email_auth/migration.sql
@@ -7,9 +7,9 @@ ALTER TABLE "User" ADD COLUMN "resetTokenHash" TEXT;
 ALTER TABLE "User" ADD COLUMN "resetExpiresAt" TIMESTAMP(3);
 UPDATE "User" SET "emailVerified" = true WHERE "googleId" IS NOT NULL;
 CREATE TABLE "Session" (
-  "sid" VARCHAR(255) NOT NULL,
+  "sid" VARCHAR NOT NULL,
   "sess" JSON NOT NULL,
   "expire" TIMESTAMP(6) NOT NULL,
-  CONSTRAINT "Session_pkey" PRIMARY KEY ("sid")
+  CONSTRAINT "session_pkey" PRIMARY KEY ("sid")
 );
-CREATE INDEX "Session_expire_idx" ON "Session"("expire");
+CREATE INDEX "IDX_session_expire" ON "Session"("expire");
