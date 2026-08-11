c1839b0a407ac92728dbaf2ae0afa6d228099fa8
andrestolher <andrestolher@gmail.com>
2026-08-11 14:37:17 -0600

    Keep persistent session table in Prisma schema

--- server/src/prisma/migrations/20260811000000_add_email_auth/migration.sql
+++ server/src/prisma/migrations/20260811000000_add_email_auth/migration.sql
@@ -6,3 +6,10 @@ ALTER TABLE "User" ADD COLUMN "verificationExpiresAt" TIMESTAMP(3);
 ALTER TABLE "User" ADD COLUMN "resetTokenHash" TEXT;
 ALTER TABLE "User" ADD COLUMN "resetExpiresAt" TIMESTAMP(3);
 UPDATE "User" SET "emailVerified" = true WHERE "googleId" IS NOT NULL;
+CREATE TABLE "Session" (
+  "sid" VARCHAR(255) NOT NULL,
+  "sess" JSON NOT NULL,
+  "expire" TIMESTAMP(6) NOT NULL,
+  CONSTRAINT "Session_pkey" PRIMARY KEY ("sid")
+);
+CREATE INDEX "Session_expire_idx" ON "Session"("expire");
